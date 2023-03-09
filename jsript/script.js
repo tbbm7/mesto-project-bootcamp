c6cdf5c291404f0a0e3a935b1d23b74c65f6e544
tbbm7 <tbbm7@yandex.ru>
2023-03-09 16:34:46 +0200

    Mesto Project sprint5 v1 верстка

---
+++ jsript/script.js
@@ -0,0 +1,28 @@
+﻿const editButton = document.querySelector('.profile__edit-button');
+const profileUser = document.querySelector('.profile__title');
+const profileAbout = document.querySelector('.profile__subtitle');
+const popUp = document.querySelector('.popup');
+const popUpClose = document.querySelector('.popup__button-close');
+const popUpSave = document.querySelector('.popup__button');
+const popUpForm = document.querySelector('.popup__form');
+const user = popUpForm.elements[0];
+const about = popUpForm.elements[1];
+
+function openPopup() {
+    popUp.classList.add('popup_is-opened');
+}
+
+function closePopup() {
+    popUp.classList.remove('popup_is-opened');
+  }
+
+function changePopup(evt) {
+    evt.preventDefault();
+    profileUser.innerText = user.value;
+    profileAbout.innerText = about.value;
+    closePopup();
+  }
+
+editButton.addEventListener('click', openPopup);
+popUpClose.addEventListener('click', closePopup);
+popUpSave.addEventListener('click', changePopup);
